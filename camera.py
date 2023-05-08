745a75efff5509b3729db5dd30977a93df26ae4b
Vladislav <vlad079@ya.ru>
2023-05-08 19:49:45 +0100

    image

--- camera.py
+++ camera.py
@@ -1,11 +1,18 @@
 from picamera import PiCamera
 from time import sleep
 
+
 class Camera:
     def __init__(self):
         self.camera = PiCamera()
 
-    def record(self, filename='video', duration=5):
+    def record_video(self, filename='video', duration=5):
         self.camera.start_recording(f'{filename}.h264')
         sleep(duration)
         self.camera.stop_recording()
+
+    def make_pic(self, filename='image'):
+        sleep(5)
+        filename = f'{filename}.jpg'
+        self.camera.capture(filename)
+        return filename
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from camera import Camera
-
+from datetime import datetime
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     camera = Camera()
-    camera.record()
+    filename = camera.make_pic(f'image-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}')
+    print(filename)
 
