c20a0484dc756776c349adc59e6762313c6c309f
Vladislav <vlad079@ya.ru>
2023-05-08 19:27:32 +0100

    test

--- camera.py
+++ camera.py
@@ -1,9 +1,11 @@
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
+class Camera:
+    def __init__(self):
+        self.camera = PiCamera()
 
-camera.start_preview()
-sleep(5)
-camera.capture('../image.jpg')
-camera.stop_preview()
+    def record(self, filename='video', duration=5):
+        self.camera.start_recording(f'{filename}.h264')
+        sleep(duration)
+        self.camera.stop_recording()
